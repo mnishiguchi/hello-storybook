b0cfe76b5608a4ea239141ac37b22b0e62a06563
Masatoshi N <nishiguchi.masa@gmail.com>
2022-07-30 21:16:30 -0400

    Import bootstrap from node_modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,35 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  core: {
+    builder: 'webpack5'
+  },
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
   ],
-  "framework": "@storybook/react"
+  framework: '@storybook/react',
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push(
+      // https://webpack.js.org/loaders/sass-loader/
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../')
+      }
+    )
+
+    // Return the altered config
+    return config
+  }
 }
